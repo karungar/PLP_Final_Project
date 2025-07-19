05b72ad1efdafe6057506d68da86cafdbed66627
Sylvia <sylvia.waweru@gmail.com>
2025-07-19 15:02:33 +0300

    dotenv configured

--- server/server.js
+++ server/server.js
@@ -53,5 +53,5 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
